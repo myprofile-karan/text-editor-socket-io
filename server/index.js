db5329b82cbf0a683cf0c8d5e340bd1fc24b0305
myprofile-karan <vkaran242@gmail.com>
2024-07-29 20:21:30 +0530

    fix: index file in server side

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import { createServer } from "http";
 
 const PORT = 3001;
 const app = express();
-const server = new createServer(app);
+const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
@@ -13,18 +13,45 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("get-document", (id) => {
-    const data = "";
-    socket.join(id);
-    socket.emit("load-document", data);
+// Object to maintain users in each room
+const rooms = {};
 
-    socket.on("send-changes", (delta) => {
-      socket.broadcast.to(id).emit("receive-changes", delta);
+io.on("connection", (socket) => {
+  // Listen for join-room event to track user count
+  socket.on("join-room", ({ roomId, userName }) => {
+    socket.join(roomId);
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+    rooms[roomId].push(userName);
+
+    // Emit the user count to the room
+    io.to(roomId).emit("user-count", rooms[roomId].length);
+
+    // Handle user disconnect
+    socket.on("disconnect", () => {
+      rooms[roomId] = rooms[roomId].filter((user) => user !== userName);
+      io.to(roomId).emit("user-count", rooms[roomId].length);
+    });
+
+    // Handle document editing events
+    socket.on("get-document", (id) => {
+      const data = "";
+      socket.join(id);
+      socket.emit("load-document", data);
+
+      socket.on("send-changes", (delta) => {
+        socket.broadcast.to(id).emit("receive-changes", delta);
+      });
+
+      socket.on("send-message", (message) => {
+        io.to(id).emit("receive-message", message);
+      });
     });
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`server is listening on ${PORT}`);
+  console.log(`Server is listening on ${PORT}`);
 });
